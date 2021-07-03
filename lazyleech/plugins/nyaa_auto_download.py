1f2594355e4743548974a71284b5e8e6a89851c4
sherlockia2 <74177321+sherlockia2@users.noreply.github.com>
2021-07-03 16:08:11 +0530

    Update nyaa_auto_download.py

--- lazyleech/plugins/nyaa_auto_download.py
+++ lazyleech/plugins/nyaa_auto_download.py
@@ -11,7 +11,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from .. import app, ADMIN_CHATS, ForceDocumentFlag
 from .leech import initiate_torrent
 
-rsslink = ["https://nyaa.si/?page=rss&u=SmallSizedAnimations","https://nyaa.si/?page=rss&q=%5BSubsPlease%5D+Boku+no+Hero+Academia+480p&c=1_2&f=0", "https://nyaa.si/?page=rss&q=%5BSubsPlease%5D+one+piece+480p&c=1_2&f=0"]
+rsslink = ["https://nyaa.si/?page=rss&q=%5Bssa%5D+720p&c=1_2&f=0","https://nyaa.si/?page=rss&q=%5BSubsPlease%5D+Boku+no+Hero+Academia+480p&c=1_2&f=0", "https://nyaa.si/?page=rss&q=%5BSubsPlease%5D+one+piece+480p&c=1_2&f=0"]
 
 if os.environ.get('DB_URL'):
     DB_URL = os.environ.get('DB_URL')
